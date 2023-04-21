53ed6775cf6fcec8fa157a2db96f2f5e20d8de1a
Luke <0x1uke@pm.me>
2023-04-20 21:27:42 -0400

    Team server updates

---
+++ TeamServer/Models/HttpListener.cs
@@ -0,0 +1,54 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TeamServer.Models;
+
+public class HttpListener : Listener
+{
+    public override string Name { get; }
+    
+    public int BindPort { get; }
+
+    private CancellationTokenSource _tokenSource;
+    
+    public HttpListener(string name)
+    {
+        Name = name;
+        BindPort = BindPort;
+    }
+    
+    public override async Task Start()
+    {
+        var hostBuilder = new HostBuilder()
+            .ConfigureWebHostDefaults(host =>
+            {
+                host.UseUrls($"http://0.0.0.0/:{BindPort}");
+                host.Configure(ConfigureApp);
+                host.ConfigureServices(ConfigureServices);
+            });
+
+        var host = hostBuilder.Build();
+
+        _tokenSource = new CancellationTokenSource();
+        host.RunAsync(_tokenSource.Token);
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+    
+    private void ConfigureApp(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(e =>
+        {
+            e.MapControllerRoute("/", "/", new { controller = "HttpListener", action = "ImplantHandler" });
+        });
+    }
+
+    public override void Stop()
+    {
+        _tokenSource.Cancel();
+    }
+}
